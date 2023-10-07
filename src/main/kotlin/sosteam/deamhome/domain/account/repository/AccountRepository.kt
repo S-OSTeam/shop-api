a5fde5ae449110c53e0ceef03f83b71434b8cfdf
정민호 <alsgh846@gmail.com>
2023-10-07 18:58:10 +0900

    Squashed commit of the following:
    commit 8a9aeeb09971924d189461922046de5baaad8c9e
    Author: Hangeulkim <ckswls89@naver.com>
    Date:   Fri Oct 6 21:51:36 2023 +0900
    
        feature/add-querydsl-and-document
    
        querydsl-mongodb 추가
        account repository 추가
        test관련 gradle 추가

--- src/main/kotlin/sosteam/deamhome/domain/account/repository/AccountRepository.kt
+++ src/main/kotlin/sosteam/deamhome/domain/account/repository/AccountRepository.kt
@@ -1,10 +1,6 @@
 package sosteam.deamhome.domain.account.repository
 
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
-import org.springframework.stereotype.Repository
-import reactor.core.publisher.Mono
 import sosteam.deamhome.domain.account.entity.Account
 
-@Repository
-interface AccountRepository: ReactiveMongoRepository<Account, String> {
-}
+interface AccountRepository : ReactiveMongoRepository<Account, String>
