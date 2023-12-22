61fee30892efe959747351f4cf48d51a2e5d9986
JooHui-void <elis100@naver.com>
2023-12-23 03:13:43 +0900

    feat: wishList 추가/삭제 서비스 제작

--- src/main/kotlin/sosteam/deamhome/domain/account/exception/WishListOverflowException.kt
+++ src/main/kotlin/sosteam/deamhome/domain/account/exception/WishListOverflowException.kt
@@ -4,12 +4,12 @@ import org.springframework.graphql.execution.ErrorType
 import sosteam.deamhome.global.exception.CustomGraphQLException
 
 class WishListOverflowException(
-    errorCode: String = "WISHLIST_OVERFLOW",
-
-    @JvmField
-    @Suppress("INAPPLICABLE_JVM_FIELD")
-    override val message: String = "위시리스트의 최대 개수(100개)를 초과했습니다."
+	errorCode: String = "WISHLIST_OVERFLOW",
+	
+	@JvmField
+	@Suppress("INAPPLICABLE_JVM_FIELD")
+	override val message: String = "위시리스트의 최대 개수(100개)를 초과했습니다."
 ) :
-    CustomGraphQLException(errorCode, ErrorType.BAD_REQUEST, message) {
-    override fun getMessage(): String = super.message
+	CustomGraphQLException(errorCode, ErrorType.BAD_REQUEST, message) {
+	override fun getMessage(): String = super.message
 }
