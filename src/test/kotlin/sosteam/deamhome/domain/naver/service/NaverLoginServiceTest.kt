17b4551c56e6a6409c02c87bba7ca543ce6aef73
Kim Chan Il <43468017+chanil4129@users.noreply.github.com>
2023-12-22 19:14:32 +0900

    test: 테스트 코드 변경

--- src/test/kotlin/sosteam/deamhome/domain/naver/service/NaverLoginServiceTest.kt
+++ src/test/kotlin/sosteam/deamhome/domain/naver/service/NaverLoginServiceTest.kt
@@ -16,17 +16,12 @@ internal class NaverLoginServiceTest : BehaviorSpec() {
     @Autowired
     private lateinit var naverService: NaverService
 
-    @MockkBean
-    private lateinit var randomKeyProvider: RandomKeyProvider
-
     init {
         Given("url 패턴이 주어지면") {
             val expectedPattern =
                 "https://nid\\.naver\\.com/oauth2\\.0/authorize\\?client_id=.*&response_type=code&redirect_uri=http://localhost:8080/api/naver/callback&state=.*"
             When("naverUrl을 생성하면") {
-                every { randomKeyProvider.randomAlphabetNumber(32) } returns "randomState"
-
-                val url = naverService.naverConnect(randomKeyProvider).naverUrl
+                val url = naverService.getNaverLoginPage()
                 Then("url 패턴과 같은 형식이어야한다.") {
                     url shouldMatch Regex(expectedPattern)
                 }
