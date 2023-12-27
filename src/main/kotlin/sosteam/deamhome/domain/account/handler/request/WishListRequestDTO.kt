50434593790ff4510eef2f4e53ff01d78b258f11
JooHui-void <elis100@naver.com>
2023-12-27 21:50:05 +0900

    fix : request로 받기 추가

---
+++ src/main/kotlin/sosteam/deamhome/domain/account/handler/request/WishListRequestDTO.kt
@@ -0,0 +1,6 @@
+package sosteam.deamhome.domain.account.handler.request
+
+data class WishListRequestDTO (
+    val userId: String,
+    val itemId: String,
+)
--- src/main/kotlin/sosteam/deamhome/domain/account/resolver/WishListResolver.kt
+++ src/main/kotlin/sosteam/deamhome/domain/account/resolver/WishListResolver.kt
@@ -4,20 +4,22 @@ import org.springframework.graphql.data.method.annotation.Argument
 import org.springframework.graphql.data.method.annotation.MutationMapping
 import org.springframework.graphql.data.method.annotation.QueryMapping
 import org.springframework.web.bind.annotation.RestController
-import sosteam.deamhome.domain.account.service.wishlist.WishListService
+import sosteam.deamhome.domain.account.handler.request.WishListRequestDTO
+import sosteam.deamhome.domain.account.service.wishlist.WishListModifyService
+import sosteam.deamhome.domain.account.service.wishlist.WishListReadService
 import sosteam.deamhome.domain.item.entity.dto.response.ItemResponseDTO
-
 @RestController
 class WishListResolver (
-    private val wishListService: WishListService
+    private val wishListModifyService: WishListModifyService,
+    private val wishListReadService: WishListReadService,
 ){
     @QueryMapping
     suspend fun getWishList(@Argument userId:String, @Argument page:Int): List<ItemResponseDTO>{
-        return wishListService.getAllWishList(userId, page)
+        return wishListReadService.getAllWishList(userId, page)
     }
     @MutationMapping
-    suspend fun updateWishListItemInclude(@Argument userId: String,@Argument itemId: String ): List<String>{
-        println("updateWishListItemInclude")
-        return wishListService.addOrRemoveWishListItem(userId, itemId)
+    suspend fun updateWishListItemInclude(@Argument request: WishListRequestDTO): List<String>{
+        val (userId, itemId) = request
+        return wishListModifyService.addOrRemoveWishListItem(userId, itemId)
     }
 }
