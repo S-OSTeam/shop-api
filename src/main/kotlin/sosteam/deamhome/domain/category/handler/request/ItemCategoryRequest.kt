0ed9fbe1438bfe1fb468a8f225ccabe77dbdef42
정민호 <alsgh846@gmail.com>
2024-01-02 17:21:33 +0900

    ItmeCategory 제목 최대 길이 변경
    ItemCategory 제목 최대 길이를 10 에서 30 으로 변경

--- src/main/kotlin/sosteam/deamhome/domain/category/handler/request/ItemCategoryRequest.kt
+++ src/main/kotlin/sosteam/deamhome/domain/category/handler/request/ItemCategoryRequest.kt
@@ -9,7 +9,7 @@ import sosteam.deamhome.global.entity.DTO
 data class ItemCategoryRequest (
 
     @field: NotBlank(message = "제목은 필수 입력 항목입니다.")
-    @field: Size(max = 10, message = "제목 최대 길이는 10 입니다.")
+    @field: Size(max = 30, message = "제목 최대 길이는 30 입니다.")
     val title: String,
     @field: Min(1L)
     val parentPublicId: Long?
