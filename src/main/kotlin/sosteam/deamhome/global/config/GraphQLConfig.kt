3d76580c4f540128122fb1bf533fa7a038ae2ce0
정민호 <alsgh846@gmail.com>
2024-01-07 00:13:10 +0900

    graphql 주석 추가
    graphql 주석 추가

--- src/main/kotlin/sosteam/deamhome/global/config/GraphQLConfig.kt
+++ src/main/kotlin/sosteam/deamhome/global/config/GraphQLConfig.kt
@@ -15,12 +15,13 @@ class GraphQLConfig {
 	@Bean
 	fun runtimeWiringConfigurer(): RuntimeWiringConfigurer {
 		return RuntimeWiringConfigurer { wiringBuilder: RuntimeWiring.Builder ->
+			// 검증이 실패했을 때 null 반환하도록 설정
 			val validationRules = ValidationRules.newValidationRules()
 				.onValidationErrorStrategy(OnValidationErrorStrategy.RETURN_NULL)
 				.build()
-
+			// graphql 스키마로 validation 하도록 설정
 			val schemaWiring = ValidationSchemaWiring(validationRules)
-
+			// graphql Scalar 타입 설정
 			wiringBuilder.scalar(ExtendedScalars.DateTime).scalar(ExtendedScalars.GraphQLLong)
 
 			wiringBuilder.directiveWiring(schemaWiring)
