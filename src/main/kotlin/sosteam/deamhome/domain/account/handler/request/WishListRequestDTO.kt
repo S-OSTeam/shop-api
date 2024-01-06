072a7712b5c1d7fe553ffbcdb0e37375099f0ba8
JooHui-void <elis100@naver.com>
2024-01-06 20:15:53 +0900

    fix : DTO 제거

--- src/main/kotlin/sosteam/deamhome/domain/account/handler/request/WishListRequestDTO.kt
+++
@@ -1,6 +0,0 @@
-package sosteam.deamhome.domain.account.handler.request
-
-data class WishListRequestDTO (
-    val userId: String,
-    val itemId: String,
-)
--- src/main/kotlin/sosteam/deamhome/domain/account/resolver/WishListResolver.kt
+++ src/main/kotlin/sosteam/deamhome/domain/account/resolver/WishListResolver.kt
@@ -4,7 +4,7 @@ import org.springframework.graphql.data.method.annotation.Argument
 import org.springframework.graphql.data.method.annotation.MutationMapping
 import org.springframework.graphql.data.method.annotation.QueryMapping
 import org.springframework.web.bind.annotation.RestController
-import sosteam.deamhome.domain.account.handler.request.WishListRequestDTO
+import sosteam.deamhome.domain.account.handler.request.WishListRequest
 import sosteam.deamhome.domain.account.service.wishlist.WishListModifyService
 import sosteam.deamhome.domain.account.service.wishlist.WishListReadService
 import sosteam.deamhome.domain.item.entity.dto.response.ItemResponseDTO
@@ -18,7 +18,7 @@ class WishListResolver (
         return wishListReadService.getAllWishList(userId, page)
     }
     @MutationMapping
-    suspend fun updateWishListItemInclude(@Argument request: WishListRequestDTO): List<String>{
+    suspend fun updateWishListItemInclude(@Argument request: WishListRequest): List<String>{
         val (userId, itemId) = request
         return wishListModifyService.addOrRemoveWishListItem(userId, itemId)
     }
