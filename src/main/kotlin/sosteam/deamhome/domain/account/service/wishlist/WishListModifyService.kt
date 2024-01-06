692d1c8f2e79255ee8a8addf3edc58978d1a0280
JooHui-void <elis100@naver.com>
2024-01-06 20:13:39 +0900

    fix : maxWishListSize companion으로빼기

--- src/main/kotlin/sosteam/deamhome/domain/account/service/wishlist/WishListModifyService.kt
+++ src/main/kotlin/sosteam/deamhome/domain/account/service/wishlist/WishListModifyService.kt
@@ -15,8 +15,6 @@ class WishListModifyService(
 	private val itemRepository: ItemRepository,
 	private val accountValidService: AccountValidService,
 ) {
-	
-	
 	suspend fun addOrRemoveWishListItem(userId: String, itemId: String): List<String> {
 		val account = accountValidService.getAccountByUserId(userId)
 		if (account.isItemIdInWishlist(itemId)) { //이미 존재함
@@ -52,5 +50,4 @@ class WishListModifyService(
 		} ?: throw ItemNotFoundException()
 		return accountRepository.save(account).awaitSingle()
 	}
-	
 }
