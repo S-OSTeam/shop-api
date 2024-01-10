57e00ea15a5416c25a0b75974909362781d78adc
Hangeulkim <ckswls89@naver.com>
2024-01-10 20:23:17 +0900

    hotfix/cors
    hotfix/cors

---
+++ src/main/kotlin/sosteam/deamhome/global/config/CorsConfig.kt
@@ -0,0 +1,16 @@
+package sosteam.deamhome.global.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.config.CorsRegistry
+import org.springframework.web.reactive.config.WebFluxConfigurer
+
+@Configuration
+class CorsConfig : WebFluxConfigurer {
+	override fun addCorsMappings(registry: CorsRegistry) {
+		registry.addMapping("/**")
+			.allowedOrigins("http://localhost:3000", "http://localhost:8080", "https://moviethree.synology.me")
+			.allowedOriginPatterns("*")
+			.allowedMethods("GET", "POST", "PUT", "PATCH", "OPTIONS")
+			.allowCredentials(true)
+	}
+}
