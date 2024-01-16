8bfda0e2f4b36a098856a891008f930046133432
Hangeulkim <ckswls89@naver.com>
2024-01-16 20:05:43 +0900

    hotfix/postgresql-setting
    포스트그레 build gradle 추가
    어노테이션 추가
    properties 추가

--- src/main/kotlin/sosteam/deamhome/DeamhomeApplication.kt
+++ src/main/kotlin/sosteam/deamhome/DeamhomeApplication.kt
@@ -2,11 +2,13 @@ package sosteam.deamhome
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories
 import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 @EnableScheduling
+@EnableR2dbcRepositories
 @EnableAsync
 class DeamhomeApplication
 
