383ea28b0217c60a22b09d5828bc126eeb01dd59
sangcheol12 <eric2057@daum.net>
2024-01-22 01:30:26 +0900

    .
    .

--- src/main/kotlin/sosteam/deamhome/global/provider/RequestProvider.kt
+++ src/main/kotlin/sosteam/deamhome/global/provider/RequestProvider.kt
@@ -10,51 +10,53 @@ import sosteam.deamhome.global.log.ReactiveRequestContextHolder
 
 @Component
 class RequestProvider {
-	
-	companion object {
-		
-		//token을 가져오는 함수
-		suspend fun getToken(): String {
-			val headers = ReactiveRequestContextHolder.getRequest().awaitSingle().headers
-			val token = headers.getFirst(HttpHeaders.AUTHORIZATION)
-			
-			if (token.isNullOrEmpty() || !token.startsWith("DBearer "))
-				throw TokenNotFoundException()
-			
-			return token.substring(8)
-		}
-		
-		//token을 가져오는 함수
-		suspend fun getRefreshToken(): String {
-			val headers = ReactiveRequestContextHolder.getRequest().awaitSingle().headers
-			val token = headers.getFirst("Authorization-Refresh")
-			
-			if (token.isNullOrEmpty() || !token.startsWith("DBearer "))
-				throw TokenNotFoundException()
-			
-			return token.substring(8)
-		}
-		
-		//mac주소를 가져오는 함수
-		suspend fun getMac(): String {
-			val headers = ReactiveRequestContextHolder.getRequest().awaitSingle().headers
-			val mac = headers.getFirst("Authorization-Mac")
-			
-			if (mac.isNullOrEmpty() || mac.length != 17) {
-				throw MacNotFoundException()
-			}
-			
-			return mac
-		}
-		// getUserAgent
-		suspend fun getAgent(): String {
-			val headers = ReactiveRequestContextHolder.getRequest().awaitSingle().headers
-			val agent = headers.getFirst("User-Agent")
 
-			if(agent.isNullOrEmpty() || (agent!="Web" && agent!="Mobile")) {
-				throw AgentNotFoundException()
-			}
-			return agent
-		}
-	}
+    companion object {
+
+        //token을 가져오는 함수
+        suspend fun getToken(): String {
+            val headers = ReactiveRequestContextHolder.getRequest().awaitSingle().headers
+            val token = headers.getFirst(HttpHeaders.AUTHORIZATION)
+
+            if (token.isNullOrEmpty() || !token.startsWith("DBearer "))
+                throw TokenNotFoundException()
+
+            return token.substring(8)
+        }
+
+        //token을 가져오는 함수
+        suspend fun getRefreshToken(): String {
+            val headers = ReactiveRequestContextHolder.getRequest().awaitSingle().headers
+            val token = headers.getFirst("Authorization-Refresh")
+
+            if (token.isNullOrEmpty() || !token.startsWith("DBearer "))
+                throw TokenNotFoundException()
+
+            return token.substring(8)
+        }
+
+        //mac주소를 가져오는 함수
+        suspend fun getMac(): String {
+            val headers = ReactiveRequestContextHolder.getRequest().awaitSingle().headers
+            val mac = headers.getFirst("Authorization-Mac")
+
+            if (mac.isNullOrEmpty() || mac.length != 17) {
+                throw MacNotFoundException()
+            }
+
+            return mac
+        }
+
+        // getUserAgent
+        suspend fun getAgent(): String {
+            val headers = ReactiveRequestContextHolder.getRequest().awaitSingle().headers
+            val agent = headers.getFirst("User-Agent")
+            println("agent = " + agent)
+
+            if (agent.isNullOrEmpty()) {
+                throw AgentNotFoundException()
+            }
+            return agent
+        }
+    }
 }
