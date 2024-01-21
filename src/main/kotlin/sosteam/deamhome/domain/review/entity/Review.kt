77c67d095ac1d20b659421152ad84884426410fa
Kim Chan Il <43468017+chanil4129@users.noreply.github.com>
2024-01-21 13:13:04 +0900

    feat: Review entity

--- src/main/kotlin/sosteam/deamhome/domain/review/entity/Review.kt
+++ src/main/kotlin/sosteam/deamhome/domain/review/entity/Review.kt
@@ -1,16 +1,9 @@
 package sosteam.deamhome.domain.review.entity
 
-import lombok.Builder
-import lombok.Setter
-import org.springframework.data.mongodb.core.mapping.Document
-import org.springframework.data.mongodb.core.mapping.DocumentReference
 import sosteam.deamhome.domain.account.entity.Account
 import sosteam.deamhome.domain.item.entity.Item
 import sosteam.deamhome.global.entity.LogEntity
-import sosteam.deamhome.global.image.entity.Image
 
-@Document
-@Builder
 class Review(
 	var title: String,
 	var content: String,
@@ -19,22 +12,5 @@ class Review(
 	var status: Boolean = false,
 	account: Account,
 	item: Item,
-	images: List<Image>
-) : LogEntity() {
-	
-	@DocumentReference(lazy = true)
-	val images: ArrayList<Image> = images as ArrayList<Image>
-	
-	@DocumentReference(lazy = true)
-	@Setter
-	var item: Item = item
-	
-	@DocumentReference(lazy = true)
-	@Setter
-	var account: Account = account
-	
-	fun addImage(image: Image): List<Image> {
-		images.add(image)
-		return images
-	}
-}
+	images: List<String>
+) : LogEntity()
