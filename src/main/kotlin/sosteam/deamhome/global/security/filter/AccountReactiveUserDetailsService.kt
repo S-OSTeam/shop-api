f9861a91dcc3e89e00ea577d8d2f41032ba8a288
정민호 <alsgh846@gmail.com>
2024-01-23 01:34:37 +0900

    migrate Mongo-> postgreSQL
    migrate Mongo-> postgreSQL

--- src/main/kotlin/sosteam/deamhome/global/security/filter/AccountReactiveUserDetailsService.kt
+++ src/main/kotlin/sosteam/deamhome/global/security/filter/AccountReactiveUserDetailsService.kt
@@ -1,21 +1,21 @@
-//package sosteam.deamhome.global.security.filter
-//
-//import kotlinx.coroutines.reactor.mono
-//import org.springframework.security.core.userdetails.ReactiveUserDetailsService
-//import org.springframework.security.core.userdetails.User
-//import org.springframework.security.core.userdetails.UserDetails
-//import org.springframework.stereotype.Service
-//import reactor.core.publisher.Mono
-//import sosteam.deamhome.domain.account.exception.AccountNotFoundException
-//import sosteam.deamhome.domain.account.repository.AccountRepository
-//
-//@Service
-//class AccountReactiveUserDetailsService(
-//	private val accountRepository: AccountRepository
-//) : ReactiveUserDetailsService {
-//	override fun findByUsername(username: String?): Mono<UserDetails> = mono {
-//		val account = accountRepository.findAccountByUserId(username!!) ?: throw AccountNotFoundException()
-//
-//		return@mono User(account.userId, null, account.getAuthorities())
-//	}
-//}
+package sosteam.deamhome.global.security.filter
+
+import kotlinx.coroutines.reactor.mono
+import org.springframework.security.core.userdetails.ReactiveUserDetailsService
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.stereotype.Service
+import reactor.core.publisher.Mono
+import sosteam.deamhome.domain.account.exception.AccountNotFoundException
+import sosteam.deamhome.domain.account.repository.AccountRepository
+
+@Service
+class AccountReactiveUserDetailsService(
+	private val accountRepository: AccountRepository
+) : ReactiveUserDetailsService {
+	override fun findByUsername(username: String?): Mono<UserDetails> = mono {
+		val account = accountRepository.findAccountByUserId(username!!) ?: throw AccountNotFoundException()
+
+		return@mono User(account.userId, null, account.getAuthorities())
+	}
+}
