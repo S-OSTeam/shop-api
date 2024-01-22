217571b22d783972372074dcddf26af328609532
Kim Chan Il <43468017+chanil4129@users.noreply.github.com>
2024-01-23 00:48:21 +0900

    refactor: pr 수정
    리뷰 점수 double->int
    입력 예외 NOT_FOUND->BAD_REQUEST
    제목 길이 10에서 30으로 수정
    리뷰 삭제할 때 이미지 삭제도 추가

--- src/main/kotlin/sosteam/deamhome/domain/review/service/ReviewDeleteService.kt
+++ src/main/kotlin/sosteam/deamhome/domain/review/service/ReviewDeleteService.kt
@@ -3,14 +3,21 @@ package sosteam.deamhome.domain.review.service
 import kotlinx.coroutines.reactor.awaitSingleOrNull
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
+import sosteam.deamhome.domain.review.exception.ReviewNotFoundException
 import sosteam.deamhome.domain.review.handler.request.ReviewDeleteRequest
 import sosteam.deamhome.domain.review.repository.ReviewRepository
+import sosteam.deamhome.global.image.provider.ImageProvider
 
 @Service
 class ReviewDeleteService(
-	private val reviewRepository: ReviewRepository
+	private val reviewRepository: ReviewRepository,
+	private val imageProvider: ImageProvider
 ) {
 	suspend fun deleteReview(request: ReviewDeleteRequest): ResponseEntity<String> {
+		val review = reviewRepository.findById(request.reviewId).awaitSingleOrNull() ?: throw ReviewNotFoundException()
+		review.images.forEach {
+			imageProvider.deleteImage(it.fileUrl)
+		}
 		reviewRepository.deleteById(request.reviewId).awaitSingleOrNull()
 		return ResponseEntity.ok("Review 제거 성공 : `${request.reviewId}` ")
 	}
