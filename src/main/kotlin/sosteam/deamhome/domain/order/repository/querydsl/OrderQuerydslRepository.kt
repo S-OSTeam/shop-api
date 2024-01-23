08df2445b7414b0f796895644415c82480c74a42
정민호 <alsgh846@gmail.com>
2024-01-23 21:59:59 +0900

    필요 없는 상속 삭제
    필요 없는 상속 삭제

--- src/main/kotlin/sosteam/deamhome/domain/order/repository/querydsl/OrderQuerydslRepository.kt
+++ src/main/kotlin/sosteam/deamhome/domain/order/repository/querydsl/OrderQuerydslRepository.kt
@@ -1,8 +1,6 @@
 package sosteam.deamhome.domain.order.repository.querydsl
 
 import com.infobip.spring.data.r2dbc.QuerydslR2dbcRepository
-import org.springframework.data.querydsl.ReactiveQuerydslPredicateExecutor
-import org.springframework.data.repository.reactive.ReactiveCrudRepository
 import sosteam.deamhome.domain.order.entity.Order
 
-interface OrderQuerydslRepository : QuerydslR2dbcRepository<Order, Long>, ReactiveQuerydslPredicateExecutor<Order>
+interface OrderQuerydslRepository : QuerydslR2dbcRepository<Order, Long>
