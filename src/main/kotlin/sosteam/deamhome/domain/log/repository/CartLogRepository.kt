7d83513ef10f9a5b4abd7b438d2818eb60181069
정민호 <alsgh846@gmail.com>
2024-01-23 21:32:34 +0900

    delete R2DBCConfiguration class
    delete R2DBCConfiguration class

--- src/main/kotlin/sosteam/deamhome/domain/log/repository/CartLogRepository.kt
+++ src/main/kotlin/sosteam/deamhome/domain/log/repository/CartLogRepository.kt
@@ -1,7 +1,9 @@
 package sosteam.deamhome.domain.log.repository
 
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
+import org.springframework.graphql.data.GraphQlRepository
 import sosteam.deamhome.domain.log.entity.CartLog
 
+@GraphQlRepository
 interface CartLogRepository: CoroutineCrudRepository<CartLog, Long> {
 }
--- src/main/kotlin/sosteam/deamhome/domain/log/repository/MailLogRepository.kt
+++ src/main/kotlin/sosteam/deamhome/domain/log/repository/MailLogRepository.kt
@@ -1,7 +1,9 @@
 package sosteam.deamhome.domain.log.repository
 
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
+import org.springframework.graphql.data.GraphQlRepository
 import sosteam.deamhome.domain.log.entity.MailLog
 
+@GraphQlRepository
 interface MailLogRepository: CoroutineCrudRepository<MailLog, Long> {
 }
