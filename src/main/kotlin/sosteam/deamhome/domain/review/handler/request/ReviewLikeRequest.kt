7446d8bdd15826dc4fad088929d3e0329f602593
Kim Chan Il <43468017+chanil4129@users.noreply.github.com>
2024-01-24 00:24:51 +0900

    feat: 좋아요 기능 추가

---
+++ src/main/kotlin/sosteam/deamhome/domain/review/handler/request/ReviewLikeRequest.kt
@@ -0,0 +1,13 @@
+package sosteam.deamhome.domain.review.handler.request
+
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
+
+data class ReviewLikeRequest(
+	@field: NotBlank(message = "Review ID는 필수 입력 항목입니다.")
+	val reviewId: String,
+	@field: NotBlank(message = "유저 ID는 필수 입력 항목입니다.")
+	val userId: String,
+	@field: NotNull
+	val like: Boolean
+)
