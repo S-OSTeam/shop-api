7bdf892871f7c448ebef5aa10774ac289a4d9f3d
Kim Chan Il <43468017+chanil4129@users.noreply.github.com>
2024-01-24 03:31:53 +0900

    feat: 리뷰 수정 기한 지정

--- src/main/kotlin/sosteam/deamhome/domain/review/service/ReviewUpdateService.kt
+++ src/main/kotlin/sosteam/deamhome/domain/review/service/ReviewUpdateService.kt
@@ -3,10 +3,13 @@ package sosteam.deamhome.domain.review.service
 import kotlinx.coroutines.reactor.awaitSingle
 import org.springframework.stereotype.Service
 import sosteam.deamhome.domain.review.exception.ReviewNotFoundException
+import sosteam.deamhome.domain.review.exception.ReviewUpdateExpiredException
 import sosteam.deamhome.domain.review.handler.request.ReviewUpdateRequest
 import sosteam.deamhome.domain.review.handler.response.ReviewResponse
 import sosteam.deamhome.domain.review.repository.ReviewRepository
 import sosteam.deamhome.global.image.provider.ImageProvider
+import java.time.LocalDateTime
+import java.time.temporal.ChronoUnit
 
 @Service
 class ReviewUpdateService(
@@ -15,6 +18,14 @@ class ReviewUpdateService(
 ) {
 	suspend fun updateReview(request: ReviewUpdateRequest): ReviewResponse {
 		val originReview = reviewRepository.findById(request.reviewId).awaitSingle() ?: throw ReviewNotFoundException()
+		
+		val createdAt = originReview.getCreatedAt()
+		val now = LocalDateTime.now()
+		
+		// 리뷰 생성 후 한달이 지났는지 확인
+		if (ChronoUnit.MONTHS.between(createdAt, now) >= 1) {
+			throw ReviewUpdateExpiredException()
+		}
 		
 		val updatedImage = originReview.images
 		// DB에 있는 Images와 비교해서 없으면 제거
