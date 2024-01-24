ce3b70aeda52bb005237f17ebab445fe755b3ab5
Kim Chan Il <43468017+chanil4129@users.noreply.github.com>
2024-01-24 18:45:10 +0900

    refactor : 피드백
    체크 로직 validation으로 분리
    graphqls "!" 제거
    querydsl의 in을 써서 코드 간결화

--- src/main/kotlin/sosteam/deamhome/domain/review/exception/ProductUsageTimeNotReachedException.kt
+++ src/main/kotlin/sosteam/deamhome/domain/review/exception/ProductUsageTimeNotReachedException.kt
@@ -3,12 +3,12 @@ package sosteam.deamhome.domain.review.exception
 import org.springframework.graphql.execution.ErrorType
 import sosteam.deamhome.global.exception.CustomGraphQLException
 
-class ReviewUpdateExpiredException(
-	errorCode: String = "UPDATE_DATE_EXPIRED",
+class ProductUsageTimeNotReachedException(
+	errorCode: String = "PRODUCT_USAGE_TIME_NOT_REACHED",
 	
 	@JvmField
 	@Suppress("INAPPLICABLE_JVM_FIELD")
-	override val message: String = "리뷰 수정 기한을 지났습니다."
+	override val message: String = "제품을 사용한지 한달이 지나지 않았습니다."
 ) : CustomGraphQLException(errorCode, ErrorType.BAD_REQUEST, message) {
 	override fun getMessage(): String = super.message
 }
