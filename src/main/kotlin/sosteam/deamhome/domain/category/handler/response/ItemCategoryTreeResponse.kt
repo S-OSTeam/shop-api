3364ac64d323c840337c2eca7eb94e0ea16604aa
정민호 <alsgh846@gmail.com>
2024-01-25 20:48:46 +0900

    add publicId to ItemCategoryTreeResponse
    add publicId to ItemCategoryTreeResponse

--- src/main/kotlin/sosteam/deamhome/domain/category/handler/response/ItemCategoryTreeResponse.kt
+++ src/main/kotlin/sosteam/deamhome/domain/category/handler/response/ItemCategoryTreeResponse.kt
@@ -3,12 +3,14 @@ package sosteam.deamhome.domain.category.handler.response
 import sosteam.deamhome.domain.category.entity.ItemCategory
 
 class ItemCategoryTreeResponse (
+    val publicId: String,
     val title: String,
     val children: MutableList<ItemCategoryTreeResponse> = mutableListOf()
 ) {
     companion object {
         fun fromItemCategory(itemCategory: ItemCategory): ItemCategoryTreeResponse {
             return ItemCategoryTreeResponse(
+                publicId = itemCategory.publicId,
                 title = itemCategory.title,
                 children = mutableListOf()
             )
