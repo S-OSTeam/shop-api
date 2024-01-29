7f9dd48f9a4118f387112056ff6df93549826f63
Kim Chan Il <43468017+chanil4129@users.noreply.github.com>
2024-01-29 13:47:44 +0900

    feat: order

--- src/main/kotlin/sosteam/deamhome/domain/order/entity/Order.kt
+++ src/main/kotlin/sosteam/deamhome/domain/order/entity/Order.kt
@@ -3,19 +3,35 @@ package sosteam.deamhome.domain.order.entity
 import org.springframework.data.annotation.Id
 import org.springframework.data.relational.core.mapping.Table
 import sosteam.deamhome.global.attribute.OrderStatus
+import sosteam.deamhome.global.entity.LogEntity
+import java.time.LocalDateTime
 
-// order 로 해도 괜찮으려나?
-@Table("order")
+@Table("order-shop")
 class Order(
 	@Id
 	var id: Long?,
+	var publicId: String,
 	val address: String,
+	val mainDoorPassWord: String,
 	val userName: String,
+	var userNickname: String,
 	val email: String,
 	val phone: String,
+	var memo: String,
+	val userId: String,
+	var totalPrice: Int,
 	val content: String,
-	var accountId: String,
-) {
+	var payType: String,
+	val appNo: String,
+	val bankInfo: String,
+	val paymentDateTime: LocalDateTime,
+	val refundDateTime: LocalDateTime,
+	val approveDatetime: LocalDateTime,
+	val couponName: String,
+	val couponPrice: Int,
+	val pointPrice: Int,
+	val downloadDays: LocalDateTime
+) : LogEntity() {
 	
 	var orderStatus: OrderStatus = OrderStatus.PENDING
 	
@@ -24,9 +40,18 @@ class Order(
 	var isPaid = false
 	var isRefund = false
 	
-	private val items: ArrayList<String> = ArrayList()
+	private val items: MutableList<String> = mutableListOf()
+	private val optionItems: MutableList<String> = mutableListOf()
+	
+	@Transient
+	val itemsCount: Int = 0
 	
 	fun addItem(itemId: String): List<String> {
+		items.add(itemId)
+		return items
+	}
+	
+	fun addOptionItem(itemId: String): List<String> {
 		items.add(itemId)
 		return items
 	}
