34aa27649185457bfd5ed873aa957fa039cc4524
Hangeulkim <ckswls89@naver.com>
2024-01-29 19:29:28 +0900

    faq
    faq

--- src/main/kotlin/sosteam/deamhome/domain/faq/entity/FaqCategory.kt
+++ src/main/kotlin/sosteam/deamhome/domain/faq/entity/FaqCategory.kt
@@ -1,10 +1,19 @@
 package sosteam.deamhome.domain.faq.entity
 
+import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Column
 import org.springframework.data.relational.core.mapping.Table
-import sosteam.deamhome.global.entity.LogEntity
+import sosteam.deamhome.global.category.entity.CategoryEntity
 
 @Table("faq_category")
 class FaqCategory(
-	var title: String,
-	
-	) : LogEntity()
+	override var title: String,
+	override var publicId: String,
+	@Column("parent_public_id")
+	override var parentPublicId: String,
+	@Column("store_id")
+	var storeId: String,
+) : CategoryEntity(2) {
+	@Id
+	var id: Long? = null
+}
