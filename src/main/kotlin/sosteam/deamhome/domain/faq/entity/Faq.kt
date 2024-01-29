c2d45e1f8bc4c0a5e66ad35ac8336da63be98ec4
Hangeulkim <ckswls89@naver.com>
2024-01-29 19:29:28 +0900

    faq
    faq

--- src/main/kotlin/sosteam/deamhome/domain/faq/entity/Faq.kt
+++ src/main/kotlin/sosteam/deamhome/domain/faq/entity/Faq.kt
@@ -1,20 +1,26 @@
 package sosteam.deamhome.domain.faq.entity
 
+import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Column
 import org.springframework.data.relational.core.mapping.Table
-import sosteam.deamhome.domain.account.entity.Account
-import sosteam.deamhome.global.entity.LogEntity
+import sosteam.deamhome.global.entity.BaseEntity
 
 @Table("faq")
 class Faq(
 	var title: String,
 	var content: String,
-	var answer: String,
-	user: Account,
-	category: FaqCategory
-) : LogEntity() {
-	val user: Account = user
-	
-	lateinit var admin: Account
-	
-	var category: FaqCategory = category
+	@Column("user_id")
+	var userId: String,
+	@Column("public_id")
+	var publicId: String,
+	@Column("category_public_id")
+	var categoryPublicId: String,
+	@Column("question_id")
+	var questionId: String?,
+	@Column("store_id")
+	var storeId: String?,
+	var imageUrls: MutableList<String> = mutableListOf()
+) : BaseEntity() {
+	@Id
+	var id: Long? = null
 }
