50b3e091ecd8f562dd56b15e4ebf03338c6ce9d2
JooHui-void <elis100@naver.com>
2024-02-26 19:15:13 +0900

    feat: graphql 추가

---
+++ src/main/kotlin/sosteam/deamhome/domain/event/handler/EventResolver.kt
@@ -0,0 +1,35 @@
+package sosteam.deamhome.domain.event.handler
+
+import org.springframework.graphql.data.method.annotation.Argument
+import org.springframework.graphql.data.method.annotation.MutationMapping
+import org.springframework.graphql.data.method.annotation.QueryMapping
+import org.springframework.web.bind.annotation.RestController
+import sosteam.deamhome.domain.event.handler.request.EventRequest
+import sosteam.deamhome.domain.event.handler.response.EventInfoResponse
+import sosteam.deamhome.domain.event.handler.response.EventItemResponse
+import sosteam.deamhome.domain.event.service.EventCreateService
+import sosteam.deamhome.domain.event.service.EventReadService
+
+@RestController
+class EventResolver (
+    private val eventCreateService: EventCreateService,
+    private val eventReadService: EventReadService,
+){
+    // 이벤트 생성
+    @MutationMapping
+    suspend fun createEvent(@Argument request: EventRequest): EventInfoResponse{
+        return eventCreateService.createEvent(request)
+    }
+
+    // 현재 진행중인 이벤트 리스트 가져오기
+    @QueryMapping
+    suspend fun getEventList(): List<EventItemResponse>{
+        return eventReadService.getEventList()
+    }
+
+    // eventId 이용해서 클릭 시 상세정보 조회
+    @QueryMapping
+    suspend fun getEventInfo(@Argument eventId: Long): EventInfoResponse{
+        return eventReadService.getEventInfo(eventId)
+    }
+}
