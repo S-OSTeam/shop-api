636d4e5743a0ec8c36336e80e85cce59c8e7cc08
Kim Chan Il <43468017+chanil4129@users.noreply.github.com>
2024-02-28 21:10:13 +0900

    refactor: review 좋아요 모듈화

---
+++ src/main/kotlin/sosteam/deamhome/domain/review/provider/ReviewLikeProvider.kt
@@ -0,0 +1,37 @@
+package sosteam.deamhome.domain.review.provider
+
+import org.springframework.stereotype.Component
+import sosteam.deamhome.domain.log.entity.ReviewLikeLog
+import sosteam.deamhome.domain.log.repository.ReviewLikeLogRepository
+import sosteam.deamhome.domain.review.entity.Review
+import sosteam.deamhome.domain.review.repository.ReviewRepository
+
+@Component
+class ReviewLikeProvider(
+	private val reviewLikeLogRepository: ReviewLikeLogRepository,
+	private val reviewRepository: ReviewRepository
+) {
+	suspend fun updateReviewLike(userId: String, reviewId: String, favor: Boolean): Review {
+		val review = reviewRepository.findByPublicId(reviewId)
+		val userLiked = review.likeUsers.contains(userId)
+		
+		if (favor && !userLiked) {
+			review.likeUsers.add(userId)
+		} else if (!favor && userLiked) {
+			review.likeUsers.remove(userId)
+		}
+		
+		reviewRepository.save(review)
+		
+		val reviewLikeLog = ReviewLikeLog(
+			id = null,
+			reviewId = review.publicId,
+			userId = userId,
+			itemId = review.itemId,
+			favor = favor
+		)
+		reviewLikeLogRepository.save(reviewLikeLog)
+		
+		return review
+	}
+}
--- src/main/kotlin/sosteam/deamhome/domain/review/service/ReviewLikeService.kt
+++ src/main/kotlin/sosteam/deamhome/domain/review/service/ReviewLikeService.kt
@@ -3,38 +3,18 @@ package sosteam.deamhome.domain.review.service
 import org.springframework.stereotype.Service
 import sosteam.deamhome.domain.account.exception.AccountNotFoundException
 import sosteam.deamhome.domain.account.repository.AccountRepository
-import sosteam.deamhome.domain.log.entity.ReviewLikeLog
-import sosteam.deamhome.domain.log.repository.ReviewLikeLogRepository
 import sosteam.deamhome.domain.review.handler.request.ReviewLikeRequest
 import sosteam.deamhome.domain.review.handler.response.ReviewResponse
-import sosteam.deamhome.domain.review.repository.ReviewRepository
+import sosteam.deamhome.domain.review.provider.ReviewLikeProvider
 
 @Service
 class ReviewLikeService(
-	private val reviewRepository: ReviewRepository,
 	private val accountRepository: AccountRepository,
-	private val reviewLikeLogRepository: ReviewLikeLogRepository
+	private val reviewLikeProvider: ReviewLikeProvider
 ) {
 	suspend fun updateReviewLike(request: ReviewLikeRequest): ReviewResponse {
 		accountRepository.findAccountByUserId(request.userId) ?: throw AccountNotFoundException()
-		val review = reviewRepository.findByPublicId(request.reviewId)
-		val userLiked = review.likeUsers.contains(request.userId)
-		
-		if (request.favor && !userLiked) {
-			review.likeUsers.add(request.userId)
-		} else if (!request.favor && userLiked) {
-			review.likeUsers.remove(request.userId)
-		}
-		reviewRepository.save(review)
-		
-		val reviewLikeLog = ReviewLikeLog(
-			id = null,
-			reviewId = request.reviewId,
-			userId = request.userId,
-			itemId = review.itemId,
-			favor = request.favor
-		)
-		reviewLikeLogRepository.save(reviewLikeLog)
+		val review = reviewLikeProvider.updateReviewLike(request.userId, request.reviewId, request.favor)
 		
 		return ReviewResponse.fromReview(review)
 	}
