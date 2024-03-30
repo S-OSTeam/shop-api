32ca2cff3b1a8ca7e8724c9b8a01b3b4fef2f9e6
JooHui-void <elis100@naver.com>
2024-03-31 00:58:41 +0900

    fix: 익셉션 메세지 수정

--- src/main/kotlin/sosteam/deamhome/domain/event/exception/InvalidEventException.kt
+++ src/main/kotlin/sosteam/deamhome/domain/event/exception/InvalidEventException.kt
@@ -8,8 +8,7 @@ class InvalidEventException (
 
     @JvmField
     @Suppress("INAPPLICABLE_JVM_FIELD")
-    override val message: String = "이벤트 요청이 올바르지 않습니다. (" +
-            "Event: "
+    override val message: String = "이벤트 요청이 올바르지 않습니다."
 ):
     CustomGraphQLException(errorCode, ErrorType.BAD_REQUEST, message) {
     override fun getMessage(): String = super.message
