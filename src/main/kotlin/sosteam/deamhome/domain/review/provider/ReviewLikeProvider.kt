e4ba733651ff57253a76edbd6ff9cc00a269edd1
Kim Chan Il <43468017+chanil4129@users.noreply.github.com>
2024-04-06 16:43:04 +0900

    refactor: DB smallint로 변경

--- src/main/kotlin/sosteam/deamhome/domain/review/provider/ReviewLikeProvider.kt
+++ src/main/kotlin/sosteam/deamhome/domain/review/provider/ReviewLikeProvider.kt
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Component
 import sosteam.deamhome.domain.log.entity.ReviewLikeLog
 import sosteam.deamhome.domain.log.repository.ReviewLikeLogRepository
 import sosteam.deamhome.domain.review.entity.Review
+import sosteam.deamhome.domain.review.entity.ReviewFavor
 import sosteam.deamhome.domain.review.repository.ReviewRepository
 
 @Component
@@ -11,13 +12,13 @@ class ReviewLikeProvider(
 	private val reviewLikeLogRepository: ReviewLikeLogRepository,
 	private val reviewRepository: ReviewRepository
 ) {
-	suspend fun updateReviewLike(userId: String, reviewId: String, favor: Boolean?): Review {
+	suspend fun updateReviewLike(userId: String, reviewId: String, favor: ReviewFavor): Review {
 		val review = reviewRepository.findByPublicId(reviewId)
 		val userLiked = review.likeUsers.contains(userId)
 		
-		if (favor == true && !userLiked) {
+		if (favor == ReviewFavor.LIKE && !userLiked) {
 			review.likeUsers.add(userId)
-		} else if ((favor == false || favor == null) && userLiked) {
+		} else if ((favor == ReviewFavor.DISLIKE || favor == ReviewFavor.NONE) && userLiked) {
 			review.likeUsers.remove(userId)
 		}
 		
@@ -28,7 +29,7 @@ class ReviewLikeProvider(
 			reviewId = review.publicId,
 			userId = userId,
 			itemId = review.itemId,
-			favor = favor
+			favor = favor.value
 		)
 		reviewLikeLogRepository.save(reviewLikeLog)
 		
