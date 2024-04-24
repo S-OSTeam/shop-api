7a378fa907be495bb09296b523d499e0822e402d
JooHui-void <elis100@naver.com>
2024-04-24 18:50:58 +0900

    fix: DB 매핑

--- src/main/kotlin/sosteam/deamhome/domain/store/entity/Store.kt
+++ src/main/kotlin/sosteam/deamhome/domain/store/entity/Store.kt
@@ -1,14 +1,21 @@
 package sosteam.deamhome.domain.store.entity
 
 import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Column
+import org.springframework.data.relational.core.mapping.Table
 import sosteam.deamhome.global.entity.BaseEntity
 
+@Table("store")
 data class Store (
     @Id
     var id: Long?,
+
+    @Column("store_name")
     val storeName: String,
     val description: String?,
     val address: String,
+
+    @Column("business_license_number")
     val businessLicenseNumber: String,
     val image: String?,
 ) : BaseEntity(){
