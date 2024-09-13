3370dcb1a878c3665a05970c01f83acc15a5e817
Hangeulkim <ckswls89@naver.com>
2024-09-13 21:36:03 +0900

    임시로 item primary
    임시로 item primary

--- src/main/kotlin/sosteam/deamhome/domain/question/repository/QuestionCategoryRepository.kt
+++ src/main/kotlin/sosteam/deamhome/domain/question/repository/QuestionCategoryRepository.kt
@@ -1,14 +1,12 @@
 package sosteam.deamhome.domain.question.repository
 
 import kotlinx.coroutines.flow.Flow
-import org.springframework.context.annotation.Primary
 import org.springframework.graphql.data.GraphQlRepository
 import sosteam.deamhome.domain.question.entity.QuestionCategory
 import sosteam.deamhome.domain.question.repository.custom.QuestionCategoryRepositoryCustom
 import sosteam.deamhome.global.category.respository.CategoryRepository
 
 @GraphQlRepository
-@Primary
 interface QuestionCategoryRepository : CategoryRepository<QuestionCategory>, QuestionCategoryRepositoryCustom {
 	suspend fun deleteByPublicId(publicId: String): Long
 	
