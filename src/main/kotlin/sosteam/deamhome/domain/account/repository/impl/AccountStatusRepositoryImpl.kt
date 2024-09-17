d2268e8353a21724c92242f2a6e8221f71e37b52
Hangeulkim <ckswls89@naver.com>
2024-09-17 23:05:52 +0900

    userId와 email 동시 입력시 오류 수정
    or 조건으로 변경

--- src/main/kotlin/sosteam/deamhome/domain/account/repository/impl/AccountStatusRepositoryImpl.kt
+++ src/main/kotlin/sosteam/deamhome/domain/account/repository/impl/AccountStatusRepositoryImpl.kt
@@ -18,11 +18,11 @@ class AccountStatusRepositoryImpl(
 	private val querydsl: AccountStatusQueryDslRepository,
 ) : AccountStatusRepositoryCustom {
 	private val accountStatus = QAccountStatus.accountStatus
-
+	
 	override fun getStatusByBeforeAYear(): Flow<AccountStatus> {
 		return querydsl.findAll(accountStatus.status.eq(Status.LIVE)).asFlow()
 	}
-
+	
 	override suspend fun getStatusByUserIdOrSNS(
 		userId: String?,
 		sns: SNS,
@@ -33,37 +33,39 @@ class AccountStatusRepositoryImpl(
 			eqLoginData(sns, snsId, userId, email)
 		).awaitFirstOrNull()
 	}
-
+	
 	//로그인을 id로 하는지 user로 하는지 sns로 하는지 확인
 	fun eqLoginData(sns: SNS, snsId: String?, userId: String?, email: String?): BooleanBuilder {
 		val expr = BooleanBuilder()
-
+		
 		//SNS가 이상한 경우는 request에서 걸러진다.
 		//id와 email이 이상한 경우는 걸러지지 않으므로 비어있는지 따로 확인한다.
-
+		
 		expr.and(accountStatus.sns.stringValue().eq(sns.name))
 		if (sns == SNS.NORMAL) {
+			val exprId = BooleanBuilder()
 			if (!userId.isNullOrBlank()) {
-				expr.and(eqUserId(userId))
+				exprId.or(eqUserId(userId))
 			}
 			if (!email.isNullOrBlank()) {
-				expr.and(eqEmail(email))
+				exprId.or(eqEmail(email))
 			}
+			expr.and(exprId)
 		} else {
 			expr.and(eqSNS(snsId))
 		}
-
+		
 		return expr
 	}
-
+	
 	private fun eqSNS(snsId: String?): BooleanExpression {
 		return accountStatus.snsId.eq(snsId)
 	}
-
+	
 	private fun eqEmail(email: String?): BooleanExpression {
 		return accountStatus.email.eq(email)
 	}
-
+	
 	private fun eqUserId(userId: String?): BooleanExpression {
 		return accountStatus.userId.eq(userId)
 	}
