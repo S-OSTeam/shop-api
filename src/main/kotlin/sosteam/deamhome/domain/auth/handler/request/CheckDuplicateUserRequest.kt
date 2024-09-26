96237e5c134edd8d0c4d131aa82ad7f1ac1eb269
Hangeulkim <41067036+Hangeulkim@users.noreply.github.com>
2024-09-26 16:15:06 +0900

    Update CheckDuplicateUserRequest.kt

--- src/main/kotlin/sosteam/deamhome/domain/auth/handler/request/CheckDuplicateUserRequest.kt
+++ src/main/kotlin/sosteam/deamhome/domain/auth/handler/request/CheckDuplicateUserRequest.kt
@@ -5,6 +5,5 @@ import sosteam.deamhome.global.attribute.SNS
 data class CheckDuplicateUserRequest(
 	val email: String?,
 	val userId: String?,
-	val sns: SNS,
-	val snsCode: String?
+	val sns: SNS
 )
